64fb1cb9512ae330432c6f5de69b5ed3c548a4b5
Andrea Vasquez G <avasquezg@gmail.com>
2013-10-17 12:37:33 -0300

    Validaciones de largo de texto en modelo

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,17 @@ class Project < ActiveRecord::Base
 	has_and_belongs_to_many :researchers
 	accepts_nested_attributes_for :researchers
 	validate :require_one_researcher
-	
 	has_and_belongs_to_many :study_sites
 	accepts_nested_attributes_for :study_sites
+	validates_length_of :title, :maximum => 200
+	validate :word_count_summary
 
 	private
 	    def require_one_researcher
 	      errors.add(:base, "You must provide at least one researcher") if self.researchers.size < 1
 	    end
 
+	    def word_count_summary
+	      errors.add(:base, "El resúmen debe tener máximo 250 palabras") if self.summary.split.size > 250
+	    end
 end
